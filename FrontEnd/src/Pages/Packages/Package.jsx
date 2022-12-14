4da9e12e17146b233daa26cf5388dffe8c0b042f
AtulSahu409 <uddish0909@gmail.com>
2022-12-14 12:20:58 +0530

    368

---
+++ FrontEnd/src/Pages/Packages/Package.jsx
@@ -0,0 +1,13 @@
+import { Box, Grid } from '@chakra-ui/react'
+import React from 'react'
+
+const Package = () => {
+  return (
+    <Box>
+        <Grid ></GR>
+
+    </Box>
+  )
+}
+
+export default Package
--- FrontEnd/src/Routes/MainRoute.jsx
+++ FrontEnd/src/Routes/MainRoute.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
+import PhoneLogin from '../Pages/Login/Login';
 const MainRoute = () => {
   return (
     <div>
         <Routes>
-            {/* <Route path="/" element={<ladingpage/>}></Route> */}
+            <Route path="/" element={<PhoneLogin/>}></Route>
             {/* use routes here */}
         </Routes>
     </div>
