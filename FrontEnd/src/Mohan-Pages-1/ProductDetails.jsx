ac2543ae0e134fa6997fadcef1199ea9773a4db1
MohanM501 <mohananna501b@gmail.com>
2022-12-15 21:07:40 +0530

    edited

--- FrontEnd/src/Mohan-Pages-1/ProductDetails.jsx
+++ FrontEnd/src/Mohan-Pages-1/ProductDetails.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect,useRef } from 'react';
 import { useParams } from 'react-router-dom';
 
 import "./ProductDetails.css";
 
 
 const ProductDetails = () => {
-  const [value, setValue] = useState(0);
-  const [valueTrue, setValueTrue] = useState(false);
 
+  const ref=useRef(null);
   const params = useParams();
   const { applianceType, city, productname } = params;
   console.log(params, "params")
@@ -15,23 +14,30 @@ const ProductDetails = () => {
 
   const handleSliderValue = (val) => {
     console.log(val, "event")
-    if (val == "3") {
-      setValue(3);
-      setValueTrue(true);
-    } else if (val == "12") {
-      setValue(94);
-      setValueTrue(true);
-    } else if (val == "6") {
-      setValue(50);
-      setValueTrue(true);
+    console.log(ref.current,"ref current");
+    
+    if (val ==="3") {
+        ref.current.value=0;
+    } else if (val === "6") {
+      ref.current.value=50;
+    } else if (val ==="12") {
+      ref.current.value=100;
     }
+    
   }
-  const handleTofalse = () => {
-    setValueTrue(false);
-    setValue(0)
+  
+  const handlefirst = (e) => {
+    console.log(e.value, "valeue before");
+    if (e.value > 25 && e.value <= 75) {
+      e.value = 50;
+    }else if(e.value<=25){
+      e.value=0;
+    }else if(e.value>75){
+      e.value=100;
+    }
+    console.log(e.value, "valeue");
 
   }
-
 
   return (
     <div className="product-details-main" >
@@ -39,22 +45,18 @@ const ProductDetails = () => {
         <div className="left-side">
           <img src="https://p.rmjo.in/moodShot/qpaqpi6f-1024x512.jpg" alt="topImage" />
         </div>
+        
         <div className="right-side">
           <div>{productname}</div>
           <div id="how_long">
             How long do you want to rent this for ? (months)
             <img src={imageLogo} alt="log" />
           </div>
-          <div style={{ backgroundColor: "red" }}>
-
-          </div>
+          
           <div id="slider_product">
-            <div className={valueTrue==true?"first-slid-hide":"first-slid-show"} >
-              <input type="range" min="1" max="100" className="slider"  id="myrange" />
-            </div>
-            <div className={valueTrue===false?"first-slid-hide":"second-slid-show"}>
-              <input type="range" onChange={handleTofalse} min="1" max="100" className="slider" value={valueTrue && value + ""} id="myrange" />
-            </div>
+
+            <input type="range" ref={ref} onChange={(e)=>handlefirst(e.target)} min="1" max="100" className="slider" id="myrange" />
+
             <div className="slider-bottom" >
               <div className="slider-months">
                 <div>| &nbsp; &nbsp; &nbsp; &nbsp;</div>
@@ -69,6 +71,9 @@ const ProductDetails = () => {
                 <div onClick={() => handleSliderValue('12')}>12+</div>
               </div>
             </div>
+          </div>
+          <div>
+            Months
           </div>
 
         </div>
