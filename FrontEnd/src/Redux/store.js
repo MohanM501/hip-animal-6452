ca0acf958bd2bc515d1398f9efbcc2d3b0821c3f
masum <mr.razamasum@gmail.com>
2022-12-17 00:48:01 +0530

    added day-3

--- FrontEnd/src/Redux/store.js
+++ FrontEnd/src/Redux/store.js
@@ -3,9 +3,10 @@ import {reducer as Package} from "./Package/reducer"
 import {reducer as City} from "./City/reducer"
 import {reducer as Packagetype } from "./PackageType/reducer"
 import {reducer as CouponReducer} from "./CouponReducer/reducer"
+import {reducer as AdminReducer} from "./AdminReducer/reducer"
 import thunk from "redux-thunk"
 
-const rootReducer = combineReducers({Package, Packagetype,City,CouponReducer});
+const rootReducer = combineReducers({Package, Packagetype,City,CouponReducer,AdminReducer});
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = legacy_createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)));
 export {store} 
